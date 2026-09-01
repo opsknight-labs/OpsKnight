67ac8bb8c7ce9746b9281e8683dd92dd1a3b8827
Dushyant Rahangdale <dushyant-rahangdale@users.noreply.github.com>
2026-09-01 17:29:45 +0530

    feat(branding): use exact authentic gradient Jira logo vector from techicons

--- src/components/common/BrandLogos.tsx
+++ src/components/common/BrandLogos.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useId } from 'react';
 
 type BrandLogoProps = {
   className?: string;
@@ -33,15 +33,49 @@ export function SlackLogo({ className = 'h-4 w-4' }: BrandLogoProps) {
 }
 
 export function JiraLogo({ className = 'h-4 w-4' }: BrandLogoProps) {
+  const id = useId().replace(/:/g, '');
+  const gradA = `jira-grad-a-${id}`;
+  const gradB = `jira-grad-b-${id}`;
+
   return (
-    <svg viewBox="0 0 24 24" className={className} fill="none" aria-label="Jira logo">
+    <svg viewBox="0 0 128 128" className={className} fill="none" aria-label="Jira logo">
+      <defs>
+        <linearGradient
+          id={gradA}
+          gradientUnits="userSpaceOnUse"
+          x1="22.034"
+          y1="9.773"
+          x2="17.118"
+          y2="14.842"
+          gradientTransform="scale(4)"
+        >
+          <stop offset="0.176" stopColor="#0052cc" />
+          <stop offset="1" stopColor="#2684ff" />
+        </linearGradient>
+        <linearGradient
+          id={gradB}
+          gradientUnits="userSpaceOnUse"
+          x1="16.641"
+          y1="15.564"
+          x2="10.957"
+          y2="21.094"
+          gradientTransform="scale(4)"
+        >
+          <stop offset="0.176" stopColor="#0052cc" />
+          <stop offset="1" stopColor="#2684ff" />
+        </linearGradient>
+      </defs>
       <path
-        d="M11.53 2c0 2.4-1.97 4.35-4.4 4.35H2.73C1.22 6.35 0 7.58 0 9.08v4.4C0 15.9 1.97 17.85 4.4 17.85h4.4c1.51 0 2.73-1.22 2.73-2.72V10.73c0-2.4 1.97-4.35 4.4-4.35h4.4c1.51 0 2.73-1.22 2.73-2.73V2H11.53z"
-        fill="#0052CC"
+        d="M108.023 16H61.805c0 11.52 9.324 20.848 20.847 20.848h8.5v8.226c0 11.52 9.328 20.848 20.848 20.848V19.977A3.98 3.98 0 00108.023 16z"
+        fill="#2684ff"
       />
       <path
-        d="M11.53 7.82c0 2.4-1.97 4.35-4.4 4.35H2.73C1.22 12.17 0 13.4 0 14.9v4.4C0 21.72 1.97 23.67 4.4 23.67h4.4c1.51 0 2.73-1.22 2.73-2.72V16.55c0-2.4 1.97-4.35 4.4-4.35h4.4c1.51 0 2.73-1.22 2.73-2.73V7.82H11.53z"
-        fill="#2684FF"
+        d="M85.121 39.04H38.902c0 11.519 9.325 20.847 20.844 20.847h8.504v8.226c0 11.52 9.328 20.848 20.848 20.848V43.016a3.983 3.983 0 00-3.977-3.977z"
+        fill={`url(#${gradA})`}
+      />
+      <path
+        d="M62.219 62.078H16c0 11.524 9.324 20.848 20.848 20.848h8.5v8.23c0 11.52 9.328 20.844 20.847 20.844V66.059a3.984 3.984 0 00-3.976-3.98z"
+        fill={`url(#${gradB})`}
       />
     </svg>
   );
